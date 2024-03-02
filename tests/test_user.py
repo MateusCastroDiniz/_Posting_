259f20e6854b41a482cdc06e60f478c209054d70
Mateus Diniz <serv.mateus@gmail.com>
2024-03-02 17:52:00 -0300

    feat: Criar teste de criação de usuário

---
+++ tests/test_user.py
@@ -0,0 +1,10 @@
+import pytest
+from core.posting.factories import UserFactory
+
+@pytest.fixture
+def user_creation():
+    return UserFactory(birthday='2004-04-06')
+
+@pytest.mark.django_db
+def test_user_creation(user_creation):
+    assert user_creation.birthday == '2004-04-06'
