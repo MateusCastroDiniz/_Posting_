54b1307ff585b834a5973294df796bea5ae2517d
Mateus Diniz <serv.mateus@gmail.com>
2024-03-04 13:36:50 -0300

    feat: modelo de relacionamentos entre usuários

---
+++ core/posting/models/relations.py
@@ -0,0 +1,14 @@
+from django.db import models
+from .user import User
+from core.settings import DATETIME_FORMAT
+
+class Relation(models.Model):
+    follower = models.ForeignKey(User, related_name='follower_username', on_delete=models.CASCADE)
+    followed = models.ForeignKey(User, related_name='followed_username', on_delete=models.CASCADE)
+    since = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        ordering = ['-since']
+
+    def __str__(self):
+        return self.follower.username + ' está seguindo ' + self.followed.username + ' desde ' + str(self.since)
