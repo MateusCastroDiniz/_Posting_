efc7eb052ece58ccc3bcbf2da620c2bee1e861c3
Mateus Diniz <serv.mateus@gmail.com>
2024-03-04 13:37:21 -0300

    chore: configuração de campos de data

--- core/posting/models/comment.py
+++ core/posting/models/comment.py
@@ -1,13 +1,15 @@
 from .post import Post
 from .user import User
 from django.db import models
+from core.settings import DATETIME_FORMAT
 
 
 class Comment(models.Model):
     content = models.CharField(max_length=180, blank=False)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_on = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_on = models.DateTimeField(auto_now=True, editable=False)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
 
     def __str__(self):
         return self.content
