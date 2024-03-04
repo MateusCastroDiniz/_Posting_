111883497cbf261c019eec75fb5234d7a94a1b2b
Mateus Diniz <serv.mateus@gmail.com>
2024-03-04 10:50:22 -0300

    chore: ajuste de serializers para modelos

--- core/posting/serializers/__init__.py
+++ core/posting/serializers/__init__.py
@@ -1,4 +1,3 @@
 from .user_serializer import UserSerializer
-from .profile_serializer import ProfileSerializer
 from .post_serializer import PostSerializer
 from .comment_serializer import CommentSerializer
--- core/posting/serializers/user_serializer.py
+++ core/posting/serializers/user_serializer.py
@@ -1,16 +1,22 @@
 from rest_framework import serializers
 from core.posting.models import User
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserSerializer(serializers.ModelSerializer):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     class Meta:
         model = User
         fields = [
             'first_name',
             'last_name',
+            'username',
             'email',
-            'birthday'
+            'birthday',
+            'sex'
         ]
 
     def create(self, validated_data):
@@ -19,7 +25,10 @@ class UserSerializer(serializers.ModelSerializer):
     def update_user(self, instance, validated_data):
         instance.first_name = validated_data.get('first_name', instance.first_name)
         instance.last_name = validated_data.get('first_name', instance.last_name)
+        instance.username = validated_data.get('username', instance.username)
+        instance.sex = validated_data.get('sex', instance.sex)
         instance.email = validated_data.get('email', instance.email)
+        instance.avatar = validated_data.get('avatar', instance.avatar)
         instance.brithday = validated_data.get('birthday', instance.brithday)
         instance.save()
         return instance
