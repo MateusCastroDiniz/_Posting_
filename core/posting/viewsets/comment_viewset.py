8e1e238c3ae503cea2b108df9e44bf0ac40e5bde
Mateus Diniz <serv.mateus@gmail.com>
2024-03-08 11:57:57 -0300

    feat: upload de viewsets

--- core/posting/viewsets/comment_viewset.py
+++ core/posting/viewsets/comment_viewset.py
@@ -1,9 +1,11 @@
+from rest_framework.decorators import action
 from rest_framework.viewsets import ModelViewSet
-from ..models import Comment
-from django.shortcuts import render
+from ..models import Comment, Post
+from django.shortcuts import render, get_object_or_404, redirect
 from ..serializers import CommentSerializer
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from ..forms import CommentForm
 
 
 class CommentViewSet(ModelViewSet):
@@ -11,6 +13,31 @@ class CommentViewSet(ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_on')
     authentication_classes = [SessionAuthentication, BasicAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def create_comment(self, request, post_id):
+        post = get_object_or_404(Post, pk=post_id)
+        if request.method == 'POST':
+            form = CommentForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('feed')
+
+    @classmethod
+    @action(detail=True, methods=['post'])
+    def edit_comment(self, request, comment_id):
+        comment = get_object_or_404(Comment, pk=comment_id)
+        if comment.created_by == request.user:
+            if request.method == 'POST':
+                form = CommentForm(request.POST, instance=comment)
+                if form.is_valid():
+                    form.save()
+                    return redirect('post_detail' + comment_id)
+            else:
+                form = CommentForm(instance=comment)
+                return render(request, 'edit_comment.html', {'form': form})
+        else:
+            return redirect('post_detail')
+
 
 
 def list_comments(requests):
--- core/posting/viewsets/user_viewset.py
+++ core/posting/viewsets/user_viewset.py
@@ -1,8 +1,15 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from rest_framework import viewsets, permissions
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from ..models import User, Relation
-from django.shortcuts import render
+from rest_framework.decorators import action
+
+from ..models import User, Relation, Post
+from django.shortcuts import render, redirect, get_object_or_404
 from ..serializers import UserSerializer
+from ..forms import UserForm
+
+
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,21 +17,43 @@ class UserViewSet(viewsets.ModelViewSet):
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
+    @classmethod
+    @action(detail=True, methods=['post'])
+    def edit_user(self, request):
+        user = get_object_or_404(User, pk=request.user.pk)
+        if request.method == 'POST':
+            form = UserForm(request.POST, instance=user)
+            if form.is_valid():
+                new_username = form.cleaned_data['username']
+                if User.objects.exclude(pk=user.pk).filter(username=new_username).exists():
+                    messages.error(request, 'O username já está em uso. Escolha outro.')
+                    return redirect('edit_user', pk=request.user.pk)
+                form.save()
+                return redirect('profile')
+        else:
+            form = UserForm(instance=user)
+        return render(request, 'edit_user.html', {'form': form})
+
     def get_queryset(self):
         return User.objects.filter(pk=self.request.user.pk)
 
+
+@login_required
 def user_config(request):
     user = request.user
+    user_posts = Post.objects.filter(author=User.objects.get(id=request.user.pk))
     followers = list(map(lambda x: x.follower.username, Relation.objects.filter(followed=User.objects.get(pk=request.user.pk))))
     following = list(map(lambda x: x.followed.username, Relation.objects.filter(follower=User.objects.get(pk=request.user.pk))))
-
+    num_followers = len(followers)
+    num_following = len(following)
 
     context = {
-        'user_info': {
-            'first_name': user.username,
+            'username': user.username,
             'email': user.email,
             'followers': followers,
-            'following': following
-        }
+            'following': following,
+            'num_following': num_following,
+            'num_followers': num_followers,
+            'user_posts': user_posts,
     }
     return render(request, 'profile.html', context)
