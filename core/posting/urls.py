3c1a32ed333287019dfe23219455b1589e2d5c98
Mateus Diniz <serv.mateus@gmail.com>
2024-03-08 12:03:17 -0300

    feat: registrar url para views e viewsets

--- core/posting/urls.py
+++ core/posting/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from rest_framework import routers
 from .viewsets import user_viewset,post_viewset,comment_viewset
 from .viewsets import *
-from .viewsets.post_viewset import post_list
+from .viewsets.post_viewset import *
+from .views import *
 
 CommentRouter = routers.SimpleRouter()
 CommentRouter.register(r"comments", CommentViewSet, basename='comments')
@@ -10,12 +11,17 @@ CommentRouter.register(r"comments", CommentViewSet, basename='comments')
 # PostRouter = routers.SimpleRouter()
 # PostRouter.register(r"feed", PostViewSet, basename='feed')
 
-UserRouter = routers.SimpleRouter()
-UserRouter.register(r"account", UserViewSet, basename='account')
+# UserRouter = routers.SimpleRouter()
+# UserRouter.register(r"account", UserViewSet, basename='account')
 
 urlpatterns = [
     path('', include(CommentRouter.urls)),
-    path('', include(UserRouter.urls)),
+    # path('', )
+    # path('', include(UserRouter.urls)),
     path('profile/', user_viewset.user_config, name='profile'),
-    path('feed/', post_list, name='feed')
+    path('profile/edit', UserViewSet.edit_user, name='profile_edit'),
+    path('feed/', post_list, name='feed'),
+    path('feed/explore', explore_list, name='explore'),
+    path('feed/p/<slug:slug>/detailed', PostDetail.as_view(), name='post_detail'),
+    path('feed/<slug:slug>/edit', PostViewSet.edit_post, name='edit_post')
 ]
