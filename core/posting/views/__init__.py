106423ebbe4dd5e097e0bd822f6ea6abc36ef52a
Mateus Diniz <serv.mateus@gmail.com>
2024-03-11 19:19:11 -0300

    feat: novas views (login e landing_page)

--- core/posting/views/__init__.py
+++ core/posting/views/__init__.py
@@ -1,3 +1,4 @@
 from .login_view import login_view
 from .logout_view import logout_view
 from .post_detail import PostDetail
+from .landingpage_view import landingpage_view
---
+++ core/posting/views/landingpage_view.py
@@ -0,0 +1,7 @@
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from ..models.user import User
+
+def landingpage_view(request):
+    return render(request, 'base.html')
