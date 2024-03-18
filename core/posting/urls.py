067e6d7c9c053f183b7400dab5f111e89af52f58
Mateus Diniz <serv.mateus@gmail.com>
2024-03-18 01:55:08 -0300

    chore: registro de rotas para funções CRUD

--- core/posting/urls.py
+++ core/posting/urls.py
@@ -9,20 +9,26 @@ CommentRouter = routers.SimpleRouter()
 CommentRouter.register(r"comments", CommentViewSet, basename='comments')
 
 PostRouter = routers.SimpleRouter()
-PostRouter.register(r"feed", PostViewSet, basename='feed')
+PostRouter.register(r"posts", PostViewSet, basename='posts')
 
 UserRouter = routers.SimpleRouter()
 UserRouter.register(r"account", UserViewSet, basename='account')
 
 urlpatterns = [
-    # path('', include(CommentRouter.urls)),
-    # path('', include(PostRouter.urls)),
-    # path('', include(UserRouter.urls)),
+    path('', include(CommentRouter.urls)),
+    path('', include(PostRouter.urls)),
+    path('', include(UserRouter.urls)),
     path('', landingpage_view, name=''),
-    path('profile/', user_viewset.user_config, name='profile'),
-    path('profile/edit', UserViewSet.edit_user, name='profile_edit'),
-    path('feed/', post_list, name='feed'),
+    path('user/', user_viewset.user_config, name='user'),
+    path('user/edit', UserViewSet.edit_user, name='user_edit'),
     path('feed/explore', explore_list, name='explore'),
+    path('feed/', post_list, name='feed'),
     path('feed/p/<slug:slug>/', PostDetail.as_view(), name='post_detail'),
-    path('feed/<slug:slug>/edit', PostViewSet.edit_post, name='edit_post')
+    path('feed/create/', PostViewSet.create_post, name='create_post'),
+    path('feed/<slug:slug>/edit/', edit_post_view, name='edit_post'),
+    path('feed/<slug:slug>/update/', PostViewSet.as_view({'post': 'update_post'}), name='update_post'),
+    path('feed/<slug:slug>/delete', PostViewSet.delete_post, name='delete_post'),
+    # path('feed/<slug:slug>/update', PostViewSet.as_view({'put': 'update'}), name='update_post')
+    # path('feed/create', PostViewSet.create_post, name='create_post'),
+    # path('feed/<slug:slug>/edit', PostViewSet.edit_post, name='edit_post')
 ]
