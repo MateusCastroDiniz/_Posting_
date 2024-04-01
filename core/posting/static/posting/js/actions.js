5986fb430d7ffc146c40b8fe7f3e95d34949217d
Mateus Diniz <serv.mateus@gmail.com>
2024-04-01 01:42:18 -0300

    update de arquivos estáticos

--- core/posting/static/posting/js/actions.js
+++ core/posting/static/posting/js/actions.js
@@ -1,36 +1,3 @@
-// $(document).ready(function(){
-//     let currentStep = 1;
-
-//     $('#nextStep').on('click', function () {
-//         $('#step' + currentStep).css('display', 'none');
-//         currentStep++;
-//         $('#step' + currentStep).css('display', 'block');
-//         $('#prevStep').css('display', 'block');
-//         if (currentStep === 3) {
-//             $('#nextStep').css('display', 'none');
-//             $('#submitBtn').css('display', 'block');
-//         }
-//     });
-
-//     $('#prevStep').on('click', function () {
-//         $('#step' + currentStep).css('display', 'none');
-//         currentStep--;
-//         $('#step' + currentStep).css('display', 'block');
-//         $('#submitBtn').css('display', 'none');
-//         if (currentStep === 1) {
-//             $('#prevStep').css('display', 'none');
-//         }
-//         if (currentStep < 3) {
-//             $('#nextStep').css('display', 'block');
-//         }
-//     });
-
-//     $('input').on('focusout', function(){
-
-//     })
-
-// })
-
 $(document).ready(function () {
     let currentStep = 1;
     let progress = 33;
