98c723a11e37b2240e7d859660f8ec893508715f
Mateus Diniz <serv.mateus@gmail.com>
2024-04-08 00:18:32 -0300

    chore: update de urls

--- core/posting/urls.py
+++ core/posting/urls.py
@@ -20,11 +20,10 @@ urlpatterns = [
     path('', include(UserRouter.urls)),
     path('', landingpage_view, name=''),
     # User paths
-    path('user/', user_viewset.user_detail, name='user'),
+    path('user/<str:username>/', UserViewSet.user_detail, name='user_detail'),
     path('user/edit/<str:username>', user_viewset.edit_user, name='edit_user'),
     path('user/edit/img/profile_picture', user_viewset.edit_profile_picture, name='edit_profile_picture'),
     path('user/edit/img/remove_profipic/', user_viewset.delete_profile_picture, name='remove_profipic'),
-    path('user/u/<str:username>', UserDetail.as_view(), name='user_detail'),
     # Post paths
     path('feed/explore', explore_list, name='explore'),
     path('feed/', post_list, name='feed'),
@@ -34,5 +33,12 @@ urlpatterns = [
     path('feed/<slug:slug>/delete/', PostViewSet.delete_post, name='delete_post'),
     # Comment paths
     path('feed/c/<slug:slug>/create', CommentViewSet.create_comment, name='create_comment'),
-    path('c/<int:id>/delete/', CommentViewSet.delete_comment, name='delete_comment')
+    path('c/<int:id>/delete/', CommentViewSet.delete_comment, name='delete_comment'),
+    # Actions paths
+    path('u/follow/<str:username>/', RelationViewSet.follow_user, name='follow_user'),
+    path('u/unfollow/<str:username>/', RelationViewSet.unfollow_user, name='unfollow_user'),
+    path('u/following/list', user_following, name='following_list'),
+    path('u/find/', user_list, name='find_user'),
+    path('feed/<slug:slug>/like', LikeViewset.like_post, name='like_post'),
+    path('feed/<slug:slug>/dislike', LikeViewset.dislike, name='dislike_post'),
 ]
